6ef71cc3bddf6480c08733db6d3fc822fd71c5de
hiromipaw <silvia@nopressure.co.uk>
2016-08-25 14:56:30 +0200

    Update Dockerfile

--- scripts/sidekiq.sh
+++ scripts/sidekiq.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-slee 5 && cd cd /organicity-discovery-api;
+slee 5 && cd organicity-discovery-api;
 bundle exec skylight setup $SKYLIGHT;
 bundle exec sidekiq -e $RAILS_ENV -C config/sidekiq.yml;
