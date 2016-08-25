711ab558590f807627e320e4b09488c6bfea53d5
hiromipaw <silvia@nopressure.co.uk>
2016-08-25 08:27:43 +0200

    Updating deploy script

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 git fetch --all;
 git reset --hard origin/master;
+git submodule update --remote --init --recursive --merge;
+git submodule foreach git pull origin master;
 docker-compose stop && docker-compose build && docker-compose up -d && ./scripts/clean.sh;
