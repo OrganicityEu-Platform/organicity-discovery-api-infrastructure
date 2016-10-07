3142f09312dd356e83e824dbe1d878989ad0cf58
hiromipaw <silvia@nopressure.co.uk>
2016-10-07 16:38:32 +0200

    Opbeat deploy beat

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -4,7 +4,7 @@ git reset --hard origin/master;
 git submodule update --remote --init --recursive --merge;
 git submodule foreach git pull origin master;
 docker-compose stop && docker-compose build && docker-compose up -d && ./scripts/clean.sh;
-curl https://opbeat.com/api/v1/organizations/<organization-id>/apps/<app-id>/releases/ \
+curl https://opbeat.com/api/v1/organizations/$OPBEAT_ORG_ID/apps/$OPBEAT_APP_ID/releases/ \
     -H "Authorization: Bearer $OPBEAT_TOKEN" \
     -d rev=`git log -n 1 --pretty=format:%H` \
     -d branch=`git rev-parse --abbrev-ref HEAD` \
