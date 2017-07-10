b88cd4e37234b48031cb8223e7d2a14ca2d62603
Viktor Smari <viktorsmari@gmail.com>
2017-07-10 17:27:31 +0200

    Script should use production.yml

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+echo "Expecting a production.yml to exist!"
 git fetch --all;
 git reset --hard origin/master;
 git submodule update --remote --init --recursive --merge;
 git submodule foreach git pull origin master;
-docker-compose stop && docker-compose build && docker-compose up -d && ./scripts/clean.sh;
+docker-compose stop && docker-compose build && docker-compose -f docker-compose.yml -f production.yml up -d && ./scripts/clean.sh;
