55c9c78488ca8fc2377ef80291eed9e58a09a782
Viktor Smári <viktorsmari@users.noreply.github.com>
2017-10-31 16:34:56 +0100

    Upgrade to docker-compose syntax 2.2 (#1)
    * Update syntax to docker-compose 2.2 (from 1.0)
    
    * Remove recommender from gitmodule
    
    * Update readme. Rename docker volume (typo)
    
    * Add stdin open to use byebug while debugging
    
    * Update sub
    
    * Update subp
    
    * Remove skylight + opbeat

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 sleep 5 && cd /organicity-discovery-api;
-bundle && bundle exec skylight setup $SKYLIGHT;
+bundle;
 rake db:gis:setup;
 rails db:migrate db:seed RAILS_ENV=$RAILS_ENV;
 rm -f /organicity-discovery-api/tmp/pids/*.pid || true;
-curl https://opbeat.com/api/v1/organizations/$OPBEAT_ORG_ID/apps/$OPBEAT_APP_ID/releases/ \
-    -H "Authorization: Bearer $OPBEAT_TOKEN" \
-    -d rev=`git log -n 1 --pretty=format:%H` \
-    -d branch=`git rev-parse --abbrev-ref HEAD` \
-    -d status=completed;
 bundle exec puma -C config/puma.rb --pidfile tmp/puma.pid -S log/puma.state --control-token smartTOKEN --control tcp://0.0.0.0:9191;
