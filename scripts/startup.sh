e28f937fe404bccf1634579b0b6b21f1be39ba3a
Viktor Smari <viktorsmari@gmail.com>
2017-12-12 13:38:40 +0100

    worker container inherits from app container

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sleep 5 && cd /organicity-discovery-api;
-bundle;
 rake db:gis:setup;
 rails db:migrate db:seed RAILS_ENV=$RAILS_ENV;
 rm -f /organicity-discovery-api/tmp/pids/*.pid || true;
